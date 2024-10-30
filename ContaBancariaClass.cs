4096ba8ac1a7bbd8f4770de1319a374c3098bce7
Suzana Araujo <suzanasouza.caetano@gmail.com>
2024-10-30 14:52:44 -0300

    first commit

---
+++ ContaBancariaClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+// abstração. ex: sistema bancario
+public abstract class ContaBancaria
+{
+	public required string NumeroConta {  get; set; }
+	public decimal Saldo { get; set; }
+
+    // Método abstrato: as classes derivadas devem implementar esse método
+    public abstract void Sacar(decimal valor);
+
+    public void Depositar(decimal valor)
+	{
+		Saldo += valor;
+		Console.WriteLine($"deposito de {valor}. Saldo atual de {Saldo:C}");
+	}
+}
+
+// classe derivada da ContaBancaria
+
+public class ContaPoupança : ContaBancaria
+{
+	public override void Sacar(decimal valor)
+	{
+		if(Saldo>=valor)
+		{
+            Saldo -= valor;
+            Console.WriteLine($"saque de {valor} realizado. Saldo atual de {Saldo}");
+        }
+		else
+		{
+			Console.WriteLine($"Saldo insuficiente. Saldo atual de {Saldo}");
+		}
+		
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Curso_Filipe
+{
+    public class ContaBancariaClass
+    {
+        public static void Main()
+        {
+            ContaBancaria contaPoupanca = new ContaPoupança { NumeroConta = "ABC-00" };
+            contaPoupanca.Depositar(200);
+            contaPoupanca.Sacar(50);
+        }
+    }
+}
+
+
